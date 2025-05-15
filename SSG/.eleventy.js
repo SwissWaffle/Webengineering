3fc9db60ea5bceadd716408198b2d65a8845b7a5
SwissWaffle <nic.weber@outlook.com>
2025-05-15 11:13:18 +0200

    Update .eleventy.js

--- SSG/.eleventy.js
+++ SSG/.eleventy.js
@@ -14,8 +14,8 @@ module.exports = function(eleventyConfig) {
     return {
       dir: {
         input: "src",   // Input directory
-        includes: "_includes",
-        layouts: "_layouts",
+        includes: "/_includes",
+        layouts: "/_layouts",
         output: "_site" // Output directory
       },
       htmlTemplateEngine: 'njk',
